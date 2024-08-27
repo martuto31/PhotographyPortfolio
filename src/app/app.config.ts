10fc61bff00b22cc72419ac09cc700e049158b73
Martin Petrov <martin.petrov@ipi-soft.com>
2024-08-27 23:33:43 +0300

    Added mobile navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withComponentInputBinding, withInMemoryScrolling, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }), withComponentInputBinding(), withRouterConfig({ onSameUrlNavigation: 'reload' })),
   ],
 };
--- src/app/components/layout/navigation/navigation-mobile/navigation-mobile.component.ts
+++ src/app/components/layout/navigation/navigation-mobile/navigation-mobile.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-navigation-mobile',
   templateUrl: './navigation-mobile.component.html',
   styleUrls: ['./navigation-mobile.component.css'],
   standalone: true,
+  imports: [
+    RouterLink,
+  ],
+  animations: [
+    trigger('slideInOut', [
+      state('void', style({ transform: 'translateX(100%)' })),
+      state('*', style({ transform: 'translateX(0)' })),
+      transition(':enter', [
+        animate('300ms ease-in-out'),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in-out', style({ transform: 'translateX(100%)' })),
+      ]),
+    ]),
+  ],
 })
 
 export class NavigationMobileComponent {
+
+  public isExpanderOpen = false;
+
+  private bodyEl = document.querySelector('body') as HTMLBodyElement;
+
+  public triggerAnimation(): void {
+    this.isExpanderOpen = !this.isExpanderOpen;
+
+    this.scrollBlock();
+  }
+
+  private scrollBlock(): void {
+    if (this.isExpanderOpen) {
+      const yOffset = window.scrollY;
+
+      this.bodyEl.style.position = 'fixed';
+      this.bodyEl.style.top = '-' + yOffset + 'px';
+      this.bodyEl.style.height = 'calc(80% - 84px + ' + yOffset + 'px)';
+    } else {
+      const top = parseInt(this.bodyEl.style.top) * -1;
+
+      this.bodyEl.style.position = '';
+      this.bodyEl.style.top = '';
+      this.bodyEl.style.height = '100%';
+
+      window.scrollTo(0, top);
+    }
+  }
 
 }
