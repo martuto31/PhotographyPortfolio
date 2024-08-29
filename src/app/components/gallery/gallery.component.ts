8ffa509043948014585f70679657d2c6bd364cc3
Martin Petrov <martuto31@gmail.com>
2024-08-29 13:07:21 +0300

    WIP

---
+++ src/app/components/gallery/gallery.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css'],
+  standalone: true,
+})
+
+export class GalleryComponent {
+
+}
