f5207dc50b0b69fa5b5e6d251204b4761151d1fe
Martin Petrov <martuto31@gmail.com>
2024-09-19 01:35:27 +0300

    Integrate MatSnackbar; Refactor Contact Me

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withComponentInputBinding, withInMemoryScrolling, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,6 +8,6 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
-    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }), withComponentInputBinding(), withRouterConfig({ onSameUrlNavigation: 'reload' })),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }), withComponentInputBinding(), withRouterConfig({ onSameUrlNavigation: 'reload' })), provideAnimationsAsync(),
   ],
 };
--- src/app/components/contact-me/contact-me.component.ts
+++ src/app/components/contact-me/contact-me.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import emailjs from '@emailjs/browser';
 
 import emailJsCredentials from './../../../assets/email-js-credentials.json';
 
 interface FormControls {
-  name: [string, ((control: AbstractControl) => ValidationErrors | null)[]],
-  email: [string, ((control: AbstractControl) => ValidationErrors | null)[]],
-  message: [string, ((control: AbstractControl) => ValidationErrors | null)[]],
+  name: FormControl<string | null>;
+  email: FormControl<string | null>;
+  message: FormControl<string | null>;
 }
 
 @Component({
@@ -24,40 +26,58 @@ interface FormControls {
 export class ContactMeComponent {
 
   constructor(
+    private snackBar: MatSnackBar,
     private formBuilder: FormBuilder) {
 
     this.createForm();
     this.emailJsInit();
   }
 
-  public formGroup!: FormGroup;
+  public formGroup!: FormGroup<FormControls>;
 
   public sendEmail(): void {
     if (this.formGroup.invalid) {
+      this.formGroup.markAllAsTouched();
+      
       return;
     }
     
     const templateParams = {
-      from_name: this.formGroup.controls['name'].value,
-      from_email: this.formGroup.controls['email'].value,
-      message: this.formGroup.controls['message'].value,
+      from_name: this.formGroup.controls.name.value,
+      from_email: this.formGroup.controls.email.value,
+      message: this.formGroup.controls.message.value,
     };
 
     emailjs.send(emailJsCredentials.serviceId, emailJsCredentials.templateId, templateParams).then(
       (response) => {
         this.formGroup.reset();
+
+        this.openSnackBar('Съобщението е изпратено успешно!', 'success');
       },
       (error) => {
-
+        this.snackBar.open('Възникна грешка!', 'error');
       },
     );
   }
 
+  public hasError(controlName: string, errorName: string) {
+    return this.formGroup.get(controlName)?.hasError(errorName) && this.formGroup.get(controlName)?.touched;
+  }
+
+  private openSnackBar(message: string, type: 'success' | 'error') {
+    this.snackBar.open(message, 'x', {
+      duration: 3000,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'center',
+      panelClass: type === 'success' ? ['snackbar-success'] : ['snackbar-error'],
+    });
+  }
+
   private createForm() {
     const formControls: FormControls = {
-      name: ['', [ Validators.required ]],
-      email: ['', [ Validators.required, Validators.pattern(/^[a-zA-Z0-9_\.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+$/) ]],
-      message: ['', [ Validators.required ]],
+      name: this.formBuilder.control('', [ Validators.required ]),
+      email: this.formBuilder.control('', [ Validators.required, Validators.pattern(/^[a-zA-Z0-9_\.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+$/) ]),
+      message: this.formBuilder.control('', [ Validators.required ]),
     };
 
     this.formGroup = this.formBuilder.group(formControls);
